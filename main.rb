a51448ac1748d11ab368b372a6e02d032b86fe26
Karam Talwandi <k.talwandi@hotmail.com>
2017-07-11 13:26:50 -0400

    Add database configuration

--- main.rb
+++ main.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'sass'
 require './song'
+configure :development do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+configure :production do 
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
 configure do 
 	enable :sessions
 	set :username, 'frank' 
